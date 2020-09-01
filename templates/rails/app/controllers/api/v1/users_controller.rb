de315bee366fe79c1feae7e4ff8e2b7ee6f29096
Salvatore Ferrucci <salvatore@algonauti.com>
2020-09-01 08:32:58 +0200

    setup jsonapi-serializer

--- templates/rails/app/controllers/api/v1/users_controller.rb
+++ templates/rails/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::UsersController < Api::BaseController
   def show
-    render json: User.find(params[:id])
+    render json: serialize(User.find(params[:id]))
+  end
+
+  private
+
+  def serialize(user)
+    UserSerializer.new(user).serializable_hash.to_json
   end
 end
